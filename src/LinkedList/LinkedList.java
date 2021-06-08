b5904b6685675a76d5142f08b389486fdba987b5
Minhaz Iftekhar <mdminhaziftekhar@gmail.com>
2021-06-08 17:42:40 +0600

    3

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -47,4 +47,31 @@ public class LinkedList {
         prev_node.next = new_node;
     }
 
+    public void append(int new_data){
+        //1. Allocate the new node
+        //2. Put in the data
+        //3. Set next as null
+        Node new_node = new Node(new_data);
+
+        //4. If list is empty, then make the new as head
+        if(head == null){
+            head = new Node(new_data);
+            return;
+        }
+
+        //4. This new node is going to be the last node, so make next of it as null
+        new_node.next = null;
+
+        //5.Else traverse till the last node
+        Node last = head;
+
+        while(last.next != null){
+            last = last.next;
+        }
+
+        //6. Change the next of the last node
+        last.next = new_node;
+        return;
+    }
+
 }
